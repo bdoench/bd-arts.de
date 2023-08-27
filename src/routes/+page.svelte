76b87237bee9a9156ba26f6927d88e136f7069c9
Sm00thC0de <bjarne.1998@freenet.de>
2023-08-28 00:59:21 +0100

    Made further improvements to the navbar. Added Background images. Improved color scheme.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,79 +1,149 @@
 <script>
-	let name = "World"
+let name = "World"
 
-	let mobile = false
+let mobile = false
 
-	const navItems = [
-    { label: "Logo", href: "https://bd-arts.de" },
-    { label: "Time", href: "#" },
-    { label: "Weather", href: "/weather" },
-    { label: "Blog", href: "/blog" },
-    { label: "Subjects", href: "/subjects" },
-    { label: "About", href: "/about" },
-    { label: "Login", href: "/login" },
-    { label: "Toogle Darkmode", href: "#" },
-    { label: "Language", href: "#" }
-  ];
-
+const navItems = [{
+        label: "Logo",
+        href: "https://bd-arts.de"
+    },
+    {
+        label: "Time",
+        href: "#"
+    },
+    {
+        label: "Weather",
+        href: "/weather"
+    },
+    {
+        label: "Blog",
+        href: "/blog"
+    },
+    {
+        label: "Subjects",
+        href: "/subjects"
+    },
+    {
+        label: "About",
+        href: "/about"
+    },
+    {
+        label: "Login",
+        href: "/login"
+    },
+    {
+        label: "Toogle Darkmode",
+        href: "#"
+    },
+    {
+        label: "Language",
+        href: "#"
+    }
+];
 </script>
 
-<nav>
-	<ul>
-<!--		
-        {#each navItems as item}
-			<li><a href={item.href}>{item.label}</a></li>
-		{/each}
--->
-        <li><a href="/" class="item left">Logo</a></li>
-        <li><a href="/time" class="item left">Time</a></li>
-        <li><a href="/weather" class="item left">Weather</a></li>
-        <li><a href="/blog" class="item center">Blog</a></li>
-        <li><a href="/subjects" class="item center">Subjects</a></li>
-        <li><a href="/about" class="item center">About Me</a></li>
-        <li><a href="/login" class="item center">Login</a></li>
-        <li><a href="#" class="item right">Toggle Darkmode</a></li>
-        <li><a href="#" class="item right">Language</a></li>
+<head>
+    <title> BD-ARTS.DE</title>
+</head>
 
-    </ul>
-</nav>	
-  
-<hr>
+<body>
+    <div class="bg-img">
+        <nav>
+            <ul>
+                <li><a href="/" class="item left logo">Logo</a></li>
+                <li><a href="/time" class="item left">Time</a></li>
+                <li><a href="/weather" class="item left">Weather</a></li>
+            </ul><ul>
+                <li><a href="/blog" class="item center">Blog</a></li>
+                <li><a href="/subjects" class="item center">Subjects</a></li>
+                <li><a href="/about" class="item center">About Me</a></li>
+                <li><a href="/login" class="item center">Login</a></li>
+            </ul><ul>
+                <li><a href="/" class="item right">Toggle Darkmode</a></li>
+                <li><a href="/" class="item right">Language</a></li>
+            </ul>
+        </nav>
 
-<h1>Hello World, welcome to <a href="https://bd-arts.de">bd-arts.de</a>!</h1>
-<p>Visit the GitHub <a href="https://github.com/bdoench/bd-arts.de">repository</a> to follow the development.</p>
+        <h1>Hello World, welcome to <a href="https://bd-arts.de">bd-arts.de</a>!</h1>
+
+        <p>This site is still under construction! For further updates or if you want to follow the development, you can visit my GitHub <a href="https://github.com/bdoench/bd-arts.de">repository</a>.</p>
+    </div>
+</body>
 
 <style>
-    nav {
-        display: flex;
-        background-color: darkslategray;
-    }
+* {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    text-align: center;
+    color: antiquewhite;
+}
 
-    ul {
-        display: flex;
-        list-style: none;
-    }
+body {
+    background-color: darkslategray;
+}
 
-    li {
-        justify-content: center;
-    }
+h1 {
+    font-size: 40px;
+    padding: 40px;
+    text-shadow: 0px 0px 10px black;
+}
 
-    .item {
-        text-decoration: none;
-        color: antiquewhite;
-        padding: 10px;
-    }
+p {
+    position: absolute;
+    bottom: 0;
+    text-shadow: 0px 0px 10px black;
+}
 
-    .left {
-        margin-left: 0px;
-        text-align: left;
-    }
+.bg-img {
+    background-image: url('/bg2.jpg');
 
-    .center {
-        text-align: center;
-    }
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
 
-    .right  {
-        margin-right: 0px;
-        text-align: right;
-    }
+    min-height: 800px;
+    min-width: 500px;
+
+    position: relative;
+}
+
+nav {
+    padding: 0 0;
+    min-width: 100%;
+    min-height: 20px;
+    background-color: rgba(47, 79, 79, 0.7);
+    display: inline-flex;
+    justify-content: space-around;
+    align-items: center;
+
+    overflow: hidden;
+
+}
+
+ul {
+    margin: 0;
+    padding: 0;
+    display: contents;
+    list-style: none;
+}
+
+.item {
+    display: inline-block;
+    text-decoration: none;
+    padding-bottom: 20px;
+    padding-top: 20px;
+
+}
+
+.item:hover {
+    background-color: rgba(47, 79, 79, 0.8)
+}
+
+.left {}
+
+.center {}
+
+.right {}
 </style>
